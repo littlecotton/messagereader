1f904bb508420097df7a961d2259532cf7bd3c71
littlecotton <pilotkaan@yahoo.com>
2015-04-02 13:16:55 -0500

    start

--- src/messagereader/BackEnd.java
+++ src/messagereader/BackEnd.java
@@ -1,5 +1,28 @@
 package messagereader;
 
-public class BackEnd {
+import java.io.File;
+import java.io.FileNotFoundException;
 
+import com.sun.java_cup.internal.runtime.Scanner;
+
+public class BackEnd {
+	private String filename;
+	private String plaintext;
+	
+	public BackEnd(String filename){
+		
+	}
+	
+	public void write(String plaintext){
+		Scanner in = null;
+		try{
+		in = new Scanner(new File(filename));
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	
 }
