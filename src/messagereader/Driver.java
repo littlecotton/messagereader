7042a8ee00cf2e9fc26ad5b11f6f8032843578b7
xjy17 <drew_hobbs@yahoo.com>
2015-04-07 12:27:03 -0500

    Finished Driver
    I added the main class

--- src/messagereader/Driver.java
+++ src/messagereader/Driver.java
@@ -1,5 +1,15 @@
 package messagereader;
 
+import javax.swing.SwingUtilities;
+
 public class Driver {
 
+	
+	
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() { new FrontEnd(); }
+		});
+	}
 }
